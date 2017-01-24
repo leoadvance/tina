d4aa673118bfdf9e85fe0e0f6a18aec830762442
leoadvance <leo_advance@163.com>
2017-01-24 11:55:13 +0800

    修复升级bug

--- package/softwinner/qtlib/bin/OTA.sh
+++ package/softwinner/qtlib/bin/OTA.sh
@@ -14,6 +14,7 @@ aw_upgrade_process.sh -f -n
 #rm -rf *
 
 # 运行need todo
+rm -rf /overlay/bin/qtapp/OTA_Todo.sh
 cd /bin/qtapp
 chmod 777 OTA_Todo.sh
 ./OTA_Todo.sh
--- package/softwinner/qtlib/bin/OTA_Todo.sh
+++ package/softwinner/qtlib/bin/OTA_Todo.sh
@@ -2,6 +2,10 @@
 
 echo "OTA Todo" 
 
+
+rm -rf /overlay/bin/qtapp/IMG_Version
+rm -rf /overlay/bin/qtapp/*.sh
+
 # 删除voerlay
 cd /overlay
 
