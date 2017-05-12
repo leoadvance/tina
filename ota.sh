7eec1fe1111f899280c149ef6ed8cc291fe77c8b
leoadvance <leo_advance@163.com>
2017-05-12 19:54:38 +0800

    转移文件目录

--- ota.sh
+++ ota.sh
@@ -1,6 +1,21 @@
 if [ $1 == "SystemUpdate" ]
 	then
 	echo "do System Update"
+
+	echo "need do ota"
+	
+	# 删除原有目录etc 并备份
+	rm -rf /mnt/UDISK/etc
+	cp -f /overlay/usr/bin/qtapp/etc /mnt/UDISK/etc
+	
+	# 删除overlay 并还原etc
+	rm -rf  /overlay/usr/bin/qtapp
+	cp -f   /mnt/UDISK/etc /overlay/usr/bin/qtapp/etc
+	
+	# 删除 UDISK目录
+	#rm -rf /mnt/UDISK/
+	echo "Delete overlay"
+
 	# 升级命令
 	aw_upgrade_process.sh -f -l /mnt/UDISK
 
